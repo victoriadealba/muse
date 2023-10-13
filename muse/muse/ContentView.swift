917838640c6350bbbdbf99157d17d3586947c7b7
victoriadealba <victoriadealba@ufl.edu>
2023-10-13 18:34:47 -0400

    uploading hello world empty app

---
+++ muse/muse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  muse
+//
+//  Created by Victoria De Alba on 10/13/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ muse/muse/museApp.swift
@@ -0,0 +1,17 @@
+//
+//  museApp.swift
+//  muse
+//
+//  Created by Victoria De Alba on 10/13/23.
+//
+
+import SwiftUI
+
+@main
+struct museApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
