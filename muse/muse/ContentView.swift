974b96e4fe25504af879cf2bae40f52b37dcf519
Alyssa Foglia <alyssafog@gmail.com>
2023-10-14 11:21:12 -0400

    front page

--- muse/muse/ContentView.swift
+++ muse/muse/ContentView.swift
@@ -8,15 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var start = false
     var body: some View {
-        Text("Welcome to Muse")
-            .font(.system(size: 40))
-            .padding()
+        VStack {
+            //header
+            ZStack{
+                RoundedRectangle(cornerRadius: 0)
+                    .foregroundColor(Color.blue)
+                    .rotationEffect(Angle(degrees: 15))
+                VStack{
+                    Text("Welcome to Muse")
+                        .font(.system(size: 45))
+                        .foregroundColor(Color.white)
+                        .bold()
+                        
+                    
+                }
+                .padding(.top,30)
+            }
+            .frame(width: UIScreen.main.bounds.width * 3, height:300)
+            .offset(y: -100)
+            
+            Button("START"){ start.toggle()}
+                .foregroundColor(Color.blue)
+                .font(.title)
+                .padding()
+                .border(Color.blue, width:5)
+                .offset(y:200)
+               
+            if start{
+                Text("clicked")
+                    //.background(Color.blue)
+                    .foregroundColor(Color.blue)
+                    .font(.title)
+                    .padding()
+                    .border(Color.blue, width:5)
+                    .offset(y:200)
+                    
+            }
+               
+            //type box
+
+            
+            Spacer()
+        }
+        
     }
+     
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
-}
